69405b8431568c5bc60ef9281245453d4ada6521
fox <phurong1@gmail.com>
2023-08-10 13:32:27 +0700

    homepage init, onboard, create thread, reply thread

---
+++ lib/mongoose.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URL) return console.log('MOMGODB_URL not found');
+
+    if (isConnected) {
+        console.log('Already connected to MongoDB');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        isConnected = true;
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log(error);
+    }
+};
